a1bcca1792702bbe91aa92bc30bc1246645cbb12
Toguer <39311017+Toguer@users.noreply.github.com>
2019-05-01 00:10:38 +0200

    Update insertar.php

--- insertar.php
+++ insertar.php
@@ -9,6 +9,7 @@
 <?php 
 $v_nom=$_POST['nom'];
 $v_msg=$_POST['msg'];
+$data = date("H:i:s"); 
 PRINT $v_msg;
 print $v_nom;
 $v_string='No estan inserit el nom o el text del missatge';
@@ -16,7 +17,7 @@ include 'connexioBD.php';
 $servername="xat";
 
     $sql = "INSERT INTO missatges (usuari,text,hora)
-        VALUES ('$usuari','$text','$data')";
+        VALUES ('$v_nom','$v_msg','$data')";
 	if (mysqli_query($con, $sql)) {
 		ob_start(null, 100);
 		header("Location:index.php?missatge=$v_string");
